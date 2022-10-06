dbdd600943f93c5ffc90f5d6b67c40dbcd951531
Pragati121 <Pragati@infostride.com>
2022-10-06 19:32:30 +0530

    applied explicit wait sucessfully

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -5,6 +5,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
@@ -16,42 +17,46 @@ import java.util.List;
 
 public class LoginPage {
     WebDriver driver;
- int number;
-    By User= By.xpath("//input[contains(@class,'input_error')]");
-    By Pwd=By.xpath("//input[contains(@type,'password')]");
-    By Submit=By.xpath("//input[contains(@class,'submit-button')]");
-    By Add=By.xpath("//button[@name=\"add-to-cart-sauce-labs-backpack\"]");
-    By ShopingCArt=By.xpath("//a[@class=\"shopping_cart_link\"]");
-    By Checkout=By.xpath("//button[@name=\"checkout\"]");
+    WebDriverWait wait;
+    int number;
+    By User = By.xpath("//input[contains(@class,'input_error')]");
+    By Pwd = By.xpath("//input[contains(@type,'password')]");
+    By Submit = By.xpath("//input[contains(@class,'submit-button')]");
+    By Add = By.xpath("//button[@name=\"add-to-cart-sauce-labs-backpack\"]");
+    By ShopingCArt = By.xpath("//a[@class=\"shopping_cart_link\"]");
+    By Checkout = By.xpath("//button[@name=\"checkout\"]");
+
     public LoginPage(WebDriver driver) {
-        this.driver=driver;
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
     }
-    public void  loginpageuser()
-    {
+
+    public void loginpageuser() {
         driver.findElement(Submit).click();
         List<WebElement> objectDetails = driver.findElements(By.xpath("//div[@class='inventory_item']"));
         number = objectDetails.size();
-        Assert.assertEquals(number,6);
+        Assert.assertEquals(number, 6);
         driver.findElement(Add).click();
         driver.findElement(ShopingCArt).click();
         driver.findElement(Checkout).click();
     }
+
     public void MethodExcel() throws IOException {
-        String path = System.getProperty("user.dir")+"//src//test//java//TestData//DataBddPractice.xlsx";
+        String path = System.getProperty("user.dir") + "//src//test//java//TestData//DataBddPractice.xlsx";
         FileInputStream prop1 = null;
-        try{
+        try {
             prop1 = new FileInputStream(path);
-        }catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         XSSFWorkbook wb = new XSSFWorkbook(prop1);
         XSSFSheet sheet = wb.getSheet("Sheet1");
         String Usern = sheet.getRow(1).getCell(0).getStringCellValue();
-        String  Pass= sheet.getRow(1).getCell(1).getStringCellValue();
-        //WebDriverWait wait =new WebDriverWait(driver,Duration.ofSeconds(150000));
-        WebDriverWait waits =new WebDriverWait(driver,Duration.ofSeconds(20));
+        String Pass = sheet.getRow(1).getCell(1).getStringCellValue();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(User));
         driver.findElement(User).sendKeys(Usern);
         driver.findElement(Pwd).sendKeys(Pass);
     }
-    }
+}
 
